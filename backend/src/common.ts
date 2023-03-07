9a3098fcf3f587e2c3838eaf3d159d12a089abe8
Aidan Case <aidanzcase@gmail.com>
2023-03-06 19:56:42 -0600

    Fix issue using client for getConnection
    I don't know why this happened, but every time some code called
    getConnection, it would spawn a new client. This prevents that. Also,
    this set's the default host to be "localhost" instead of "postgres" so that
    everyone else is not surprised if it explodes with a connection error.

--- backend/src/common.ts
+++ backend/src/common.ts
@@ -1,18 +1,17 @@
-import { Client } from 'pg';
+import { Pool, PoolClient } from 'pg';
 
 // Environment Variables
 export const port: number = +(process.env.PROJECT_PORT || 5000);
 export const host: string = process.env.PROJECT_HOST || 'localhost'
-
-// Database Access Helper Functions
-export const getConnection = async (): Promise<Client> => {
-    const client = new Client({
-        host: process.env.DB_HOST ?? 'postgres',
+const pool = new Pool({
+        host: process.env.DB_HOST ?? 'localhost',
         port: +(process.env.DB_PORT ?? '5432'),
         database: process.env.DB_SCHEMA ?? 'postgres',
         user: 'postgres',
         password: process.env.DB_PASSWORD ?? 'postgres'
-    })
-    await client.connect();
-    return client;
+});
+
+// Database Access Helper Functions
+export const getConnection = async (): Promise<PoolClient> => {
+    return await pool.connect();
 };
