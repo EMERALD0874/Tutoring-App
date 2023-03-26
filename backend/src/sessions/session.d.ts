578549c2353a3bd3ef801df396ada9c2516d436f
emart870 <72898341+emart870@users.noreply.github.com>
2023-03-26 18:00:34 -0500

    Added Sessions Data Interactors and Sessions Endpoints

---
+++ backend/src/sessions/session.d.ts
@@ -0,0 +1,23 @@
+export interface Session
+{
+    id: string,
+    student_id: string,
+    tutor_id: string,
+    appointment: string,
+}
+
+export interface CreateSession
+{
+    id: string,
+    student_id: string,
+    tutor_id: string,
+    appointment: string,
+}
+
+export interface UpdateSession
+{
+    id?: string,
+    student_id?: string,
+    tutor_id?: string,
+    appointment?: string,
+}
